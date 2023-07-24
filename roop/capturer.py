265a8e41cd7e1bab13390a4475b7217d676206f0
User <david89062388@gmail.com>
2023-07-24 17:35:32 +0800

    20230724

--- roop/capturer.py
+++ roop/capturer.py
@@ -1,10 +1,8 @@
-from typing import Optional
+from typing import Any
 import cv2
 
-from roop.typing import Frame
 
-
-def get_video_frame(video_path: str, frame_number: int = 0) -> Optional[Frame]:
+def get_video_frame(video_path: str, frame_number: int = 0) -> Any:
     capture = cv2.VideoCapture(video_path)
     frame_total = capture.get(cv2.CAP_PROP_FRAME_COUNT)
     capture.set(cv2.CAP_PROP_POS_FRAMES, min(frame_total, frame_number - 1))
--- roop/processors/frame/face_enhancer.py
+++ roop/processors/frame/face_enhancer.py
@@ -1,12 +1,12 @@
-from typing import Any, List, Callable
+from typing import Any, List
 import cv2
 import threading
-from gfpgan.utils import GFPGANer
+import gfpgan
 
 import roop.globals
 import roop.processors.frame.core
 from roop.core import update_status
-from roop.face_analyser import get_many_faces
+from roop.face_analyser import get_one_face
 from roop.typing import Frame, Face
 from roop.utilities import conditional_download, resolve_relative_path, is_image, is_video
 
@@ -14,31 +14,6 @@ FACE_ENHANCER = None
 THREAD_SEMAPHORE = threading.Semaphore()
 THREAD_LOCK = threading.Lock()
 NAME = 'ROOP.FACE-ENHANCER'
-
-
-def get_face_enhancer() -> Any:
-    global FACE_ENHANCER
-
-    with THREAD_LOCK:
-        if FACE_ENHANCER is None:
-            model_path = resolve_relative_path('../models/GFPGANv1.4.pth')
-            # todo: set models path -> https://github.com/TencentARC/GFPGAN/issues/399
-            FACE_ENHANCER = GFPGANer(model_path=model_path, upscale=1, device=get_device())
-    return FACE_ENHANCER
-
-
-def get_device() -> str:
-    if 'CUDAExecutionProvider' in roop.globals.execution_providers:
-        return 'cuda'
-    if 'CoreMLExecutionProvider' in roop.globals.execution_providers:
-        return 'mps'
-    return 'cpu'
-
-
-def clear_face_enhancer() -> None:
-    global FACE_ENHANCER
-
-    FACE_ENHANCER = None
 
 
 def pre_check() -> bool:
@@ -54,43 +29,45 @@ def pre_start() -> bool:
     return True
 
 
-def post_process() -> None:
-    clear_face_enhancer()
+def get_face_enhancer() -> Any:
+    global FACE_ENHANCER
+
+    with THREAD_LOCK:
+        if FACE_ENHANCER is None:
+            model_path = resolve_relative_path('../models/GFPGANv1.4.pth')
+            # todo: set models path https://github.com/TencentARC/GFPGAN/issues/399
+            FACE_ENHANCER = gfpgan.GFPGANer(model_path=model_path, upscale=1) # type: ignore[attr-defined]
+    return FACE_ENHANCER
 
 
-def enhance_face(target_face: Face, temp_frame: Frame) -> Frame:
-    start_x, start_y, end_x, end_y = map(int, target_face['bbox'])
-    temp_face = temp_frame[start_y:end_y, start_x:end_x]
-    if temp_face.size:
-        with THREAD_SEMAPHORE:
-            _, _, temp_face = get_face_enhancer().enhance(
-                temp_face,
-                paste_back=True
-            )
-        temp_frame[start_y:end_y, start_x:end_x] = temp_face
+def enhance_face(temp_frame: Frame) -> Frame:
+    with THREAD_SEMAPHORE:
+        _, _, temp_frame = get_face_enhancer().enhance(
+            temp_frame,
+            paste_back=True
+        )
     return temp_frame
 
 
-def process_frame(source_face: Face, reference_face: Face, temp_frame: Frame) -> Frame:
-    many_faces = get_many_faces(temp_frame)
-    if many_faces:
-        for target_face in many_faces:
-            temp_frame = enhance_face(target_face, temp_frame)
+def process_frame(source_face: Face, temp_frame: Frame) -> Frame:
+    target_face = get_one_face(temp_frame)
+    if target_face:
+        temp_frame = enhance_face(temp_frame)
     return temp_frame
 
 
-def process_frames(source_path: str, temp_frame_paths: List[str], update: Callable[[], None]) -> None:
+def process_frames(source_path: str, temp_frame_paths: List[str], progress: Any = None) -> None:
     for temp_frame_path in temp_frame_paths:
         temp_frame = cv2.imread(temp_frame_path)
-        result = process_frame(None, None, temp_frame)
+        result = process_frame(None, temp_frame)
         cv2.imwrite(temp_frame_path, result)
-        if update:
-            update()
+        if progress:
+            progress.update(1)
 
 
 def process_image(source_path: str, target_path: str, output_path: str) -> None:
     target_frame = cv2.imread(target_path)
-    result = process_frame(None, None, target_frame)
+    result = process_frame(None, target_frame)
     cv2.imwrite(output_path, result)
 
 
